ba32c38745d7cd63846c63f5533b8312cbc7a2da
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-08-14 10:13:56 -0300

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -137,6 +137,8 @@ function updateCanvas() {
 }
 
 // ===================== Inicializações =====================
+document.body.classList.add('fae'); // <<< tema FAE inicial
+themeSelect.value = 'fae';          // seleciona no dropdown
 updateCanvas();
 initHackerDrops();
 
