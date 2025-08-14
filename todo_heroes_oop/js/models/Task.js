1e0d34fcfd372002485682511dc58f543ac2ffcd
by-lelaEbirds <matheusperescurriculum@gmail.com>
2025-08-14 08:41:45 -0300

    Delete todo_heroes_oop directory

--- todo_heroes_oop/js/models/Task.js
+++
@@ -1,7 +0,0 @@
-class Task {
-    constructor(text, completed = false) {
-        this.id = Date.now();
-        this.text = text;
-        this.completed = completed;
-    }
-}
--- todo_heroes_oop/js/services/Storage.js
+++
@@ -1,9 +0,0 @@
-class Storage {
-    static getTasks() {
-        return JSON.parse(localStorage.getItem('tasks')) || [];
-    }
-
-    static saveTasks(tasks) {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-}
